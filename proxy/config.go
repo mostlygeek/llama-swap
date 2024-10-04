f5c6b962affc430da773a0926d86d535a754d1d5
Benson Wong <mostlygeek@gmail.com>
2024-10-04 10:55:02 -0700

    improve error handling

--- proxy/config.go
+++ proxy/config.go
@@ -7,12 +7,32 @@ import (
 )
 
 type ModelConfig struct {
-	Cmd   string `yaml:"cmd"`
-	Proxy string `yaml:"proxy"`
+	Cmd     string   `yaml:"cmd"`
+	Proxy   string   `yaml:"proxy"`
+	Aliases []string `yaml:"aliases"`
 }
 
 type Config struct {
-	Models map[string]ModelConfig `yaml:"models"`
+	Models             map[string]ModelConfig `yaml:"models"`
+	HealthCheckTimeout int                    `yaml:"healthCheckTimeout"`
+}
+
+func (c *Config) FindConfig(modelName string) (ModelConfig, bool) {
+	modelConfig, found := c.Models[modelName]
+	if found {
+		return modelConfig, true
+	}
+
+	// Search through aliases to find the right config
+	for _, config := range c.Models {
+		for _, alias := range config.Aliases {
+			if alias == modelName {
+				return config, true
+			}
+		}
+	}
+
+	return ModelConfig{}, false
 }
 
 func LoadConfig(path string) (*Config, error) {
@@ -25,6 +45,10 @@ func LoadConfig(path string) (*Config, error) {
 	err = yaml.Unmarshal(data, &config)
 	if err != nil {
 		return nil, err
+	}
+
+	if config.HealthCheckTimeout < 15 {
+		config.HealthCheckTimeout = 15
 	}
 
 	return &config, nil
