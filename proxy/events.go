4b97168b3d1f8d8b822465f0bd43f36a87ba4552
Benson Wong <mostlygeek@gmail.com>
2025-07-01 15:27:16 -0700

    change watch configuration to events

--- proxy/events.go
+++ proxy/events.go
@@ -3,6 +3,8 @@ package proxy
 // package level registry of the different event types
 
 const ProcessStateChangeEventID = 0x01
+const ChatCompletionStatsEventID = 0x02
+const ConfigFileChangedEventID = 0x03
 
 type ProcessStateChangeEvent struct {
 	ProcessName string
@@ -12,4 +14,18 @@ type ProcessStateChangeEvent struct {
 
 func (e ProcessStateChangeEvent) Type() uint32 {
 	return ProcessStateChangeEventID
+}
+
+type ChatCompletionStats struct {
+	TokensGenerated int
+}
+
+func (e ChatCompletionStats) Type() uint32 {
+	return ChatCompletionStatsEventID
+}
+
+type ConfigFileChangedEvent struct{}
+
+func (e ConfigFileChangedEvent) Type() uint32 {
+	return ConfigFileChangedEventID
 }
