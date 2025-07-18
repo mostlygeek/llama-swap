f669140439c5fa90c2b05d3bd3b89165f8048463
T. M. <pm2mtr@gmail.com>
2025-07-18 17:10:44 +0000

    feat: add activity page and handler

---
+++ ui/src/pages/Activity.tsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+
+const ActivityPage = () => {
+  return (
+    <div>
+      <h1>Activity</h1>
+      <p>This is the activity page.</p>
+    </div>
+  );
+};
+
+export default ActivityPage;
