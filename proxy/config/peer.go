fb0bd26ee1f4388a0fd2698db86d9ec046380483
Benson Wong <mostlygeek@gmail.com>
2025-12-26 19:53:32 -0800

    proxy/config: add PeerConfig
    - add peers example to config.example.yaml
    - updated config-schema.json with new configuration schema types
    - add proxy.PeerConfig to hold new peer config info

---
+++ proxy/config/peer.go
@@ -0,0 +1,46 @@
+package config
+
+import (
+	"fmt"
+	"net/url"
+)
+
+type PeerConfig struct {
+	Proxy    string   `yaml:"proxy"`
+	ProxyURL *url.URL `yaml:"-"`
+	ApiKey   string   `yaml:"apiKey"`
+	Models   []string `yaml:"models"`
+}
+
+func (c *PeerConfig) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	type rawPeerConfig PeerConfig
+	defaults := rawPeerConfig{
+		Proxy:  "",
+		ApiKey: "",
+		Models: []string{},
+	}
+
+	if err := unmarshal(&defaults); err != nil {
+		return err
+	}
+
+	// Validate proxy is not empty
+	if defaults.Proxy == "" {
+		return fmt.Errorf("proxy is required")
+	}
+
+	// Validate proxy is a valid URL and store the parsed value
+	parsedURL, err := url.Parse(defaults.Proxy)
+	if err != nil {
+		return fmt.Errorf("invalid peer proxy URL (%s): %w", defaults.Proxy, err)
+	}
+	defaults.ProxyURL = parsedURL
+
+	// Validate models is not empty
+	if len(defaults.Models) == 0 {
+		return fmt.Errorf("peer models can not be empty")
+	}
+
+	*c = PeerConfig(defaults)
+	return nil
+}
