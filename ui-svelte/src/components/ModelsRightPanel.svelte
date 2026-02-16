43fa9156dd071859feea007e56a0c01fd7822410
carlosmolina0615 <carlosmolina0615@gmail.com>
2026-02-16 12:40:57 +0100

    fix(ui): isolate models right panel to avoid remounting recipe manager

---
+++ ui-svelte/src/components/ModelsRightPanel.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { upstreamLogs } from "../stores/api";
+  import StatsPanel from "./StatsPanel.svelte";
+  import LogPanel from "./LogPanel.svelte";
+
+  interface Props {
+    showStats: boolean;
+  }
+
+  let { showStats }: Props = $props();
+</script>
+
+<div class="flex flex-col h-full space-y-4">
+  {#if showStats}
+    <StatsPanel />
+  {/if}
+  <div class="flex-1 min-h-0">
+    <LogPanel id="modelsupstream" title="Upstream Logs" logData={$upstreamLogs} />
+  </div>
+</div>
--- ui-svelte/src/routes/Models.svelte
+++ ui-svelte/src/routes/Models.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
   import { isNarrow } from "../stores/theme";
-  import { upstreamLogs } from "../stores/api";
   import ModelsPanel from "../components/ModelsPanel.svelte";
-  import StatsPanel from "../components/StatsPanel.svelte";
-  import LogPanel from "../components/LogPanel.svelte";
+  import ModelsRightPanel from "../components/ModelsRightPanel.svelte";
   import ResizablePanels from "../components/ResizablePanels.svelte";
 
   let direction = $derived<"horizontal" | "vertical">($isNarrow ? "vertical" : "horizontal");
@@ -14,13 +12,6 @@
     <ModelsPanel />
   {/snippet}
   {#snippet rightPanel()}
-    <div class="flex flex-col h-full space-y-4">
-      {#if direction === "horizontal"}
-        <StatsPanel />
-      {/if}
-      <div class="flex-1 min-h-0">
-        <LogPanel id="modelsupstream" title="Upstream Logs" logData={$upstreamLogs} />
-      </div>
-    </div>
+    <ModelsRightPanel showStats={direction === "horizontal"} />
   {/snippet}
 </ResizablePanels>
