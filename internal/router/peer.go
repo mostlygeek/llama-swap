de587afa04897d226e73aa77cb3be55801d63b44
Benson Wong <mostlygeek@gmail.com>
2026-05-17 14:03:19 -0700

    stubs for router/peer

--- internal/router/peer.go
+++ internal/router/peer.go
@@ -27,6 +27,7 @@ type PeerRouter struct {
 
 func NewPeer(peers config.PeerDictionaryConfig, logger *logmon.Monitor) (*PeerRouter, error) {
 	// TODO: implement
+
 	return &PeerRouter{}, nil
 }
 
@@ -36,5 +37,15 @@ func (r *PeerRouter) Shutdown(timeout time.Duration) error {
 }
 
 func (r *PeerRouter) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+
 	// TODO: implement
+	/*
+		look for model in request context. If it is not there then
+		log a warning it is missing in the context and attempt to extract it from the request
+		body ourselves. if that fails, write an error response using SendError.
+
+		look it up in our peers map. Forward the request to the peer if found otherwise
+		SendResponse with a 404 that the model was
+	*/
+
 }
